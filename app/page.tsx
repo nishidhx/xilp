dd2710543420286f2ce869cf01b75f2964ecbf3d
nee-hit476 <xnishidh.codes@gmail.com>
2026-06-03 01:28:12 +0530

    added linkings

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Navigation } from "@/components/Navigation";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex justify-center p-5">
+    <div className="max-h-screen max-w-screen flex justify-center p-5">
       
     </div>
   );
