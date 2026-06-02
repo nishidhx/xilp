da5ce9d61f56b10f66f45d370cb8b91bed0b5476
nee-hit476 <xnishidh.codes@gmail.com>
2026-06-03 01:29:09 +0530

    created navigation component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -7,11 +7,11 @@ export const Navigation = ({}) => {
   const { currentTheme, toggleTheme } = useTheme();
   return (
     <div className="navigation mx-auto max-w-2xl px-4 flex h-14 items-center justify-between">
-      <div className="navigator flex flex-row gap-2 justify-center items-center">
-        <Link href={""}>Home</Link>
-        <Link href={""}>Work</Link>
-        <Link href={""}>Projects</Link>
-        <Link href={""}>Resume</Link>
+      <div className="navigator flex flex-row text-sm font-medium text-slate-50/50 gap-6 justify-center items-center hanken-grotesk">
+        <Link href={"/"}>Home</Link>
+        <Link href={"/work"}>Work</Link>
+        <Link href={"/project"}>Projects</Link>
+        <Link href={"/resume"}>Resume</Link>
       </div>
 
 
@@ -20,9 +20,9 @@ export const Navigation = ({}) => {
           onClick={toggleTheme}
           className="relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full transition-all duration-500 active:scale-90"
         >
-          {/* Sun */}
+          {/* Sun minimize logic */}
           <Sun
-            className={`absolute transition-all duration-500
+            className={`absolute transition-all duration-500 text-slate-50/50
           ${
             currentTheme === "light"
               ? "rotate-0 scale-100 opacity-100"
@@ -30,9 +30,9 @@ export const Navigation = ({}) => {
           }`}
           />
 
-          {/* Moon */}
+          {/* Moon minimize logic */}
           <Moon
-            className={`absolute transition-all duration-500
+            className={`absolute transition-all opacity-50 duration-500 text-slate-50/50
           ${
             currentTheme === "dark"
               ? "rotate-0 scale-100 opacity-100"
