8813b1ec181bc20493e944344a6321353d45cff4
nee-hit476 <xnishidh.codes@gmail.com>
2026-06-03 00:19:38 +0530

    added theme custom hook for data consumption

---
+++ hooks/theme.ts
@@ -0,0 +1,13 @@
+"use client"
+import { ThemeContext } from "@/contexts/ThemeProvider";
+import { useContext } from "react";
+
+// custom hook for data consumption
+export const useTheme = () => {
+  const themeContext = useContext(ThemeContext);
+  if (!themeContext) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+    // return;
+  }
+  return themeContext;
+};
