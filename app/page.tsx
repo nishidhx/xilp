964b55c7a23547636cb3480232a17268c4c0ac10
nee-hit476 <xnishidh.codes@gmail.com>
2026-06-03 02:44:16 +0530

    fixed layout issues for mobile view

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Hero } from "@/components/hero";
 import { Navigation } from "@/components/Navigation";
 
 export default function Home() {
   return (
-    <div className="max-h-screen max-w-screen flex justify-center p-5">
-      
+    <div className="max-h-screen md:ml-auto md:mr-auto px-2 max-w-2xl">
+        <Hero />
     </div>
   );
 }
