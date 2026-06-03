1f29b3cfc594a98e11634f80516834ed4c1773f9
nee-hit476 <xnishidh.codes@gmail.com>
2026-06-03 15:52:20 +0530

    added notify functionality

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,61 +3,55 @@ import { useTheme } from "@/hooks/theme";
 import { Search } from "@/icons/search-icon";
 import { Moon, Sun } from "@/icons/theme-icon";
 import Link from "next/link";
+import { SerachBar } from "./search-bar";
 
 export const Navigation = ({}) => {
   const { currentTheme, toggleTheme } = useTheme();
 
   return (
-    <div
-      className="navigation mx-auto https://discord.com/channels/1418625695432839342/1428711888501604423
-    max-w-screen
-    md:max-w-2xl px-4 pt-4 flex h-14 items-center justify-between bg-transparent"
-    >
-      <div className="navigator flex flex-row md:text-sm text-slate-50/50 gap-4 md:gap-6 justify-center items-center hanken-grotesk">
-        <Link href={"/"}>Home</Link>
-        <Link href={"/work"}>Work</Link>
-        <Link href={"/project"}>Projects</Link>
-        <Link href={"/resume"}>Resume</Link>
-      </div>
-
-      {/* Serach bar */}
-      <div className="flex items-center">
-        <div className="search-bar flex gap-2">
-          <div className="md:w-30 md:border flex justify-between items-center px-2 md:border-slate-50/20 rounded-2xl md:bg-slate-50/5 h-8 p-5">
-            <Search className="size-5 md:size-4 font-bold" />
-            <div className="gap-1 px-2 hidden md:flex">
-              <kbd className="bg-slate-100/10 px-1 rounded-md">Ctrl</kbd>
-              <kbd className="bg-slate-100/10 px-1 rounded-md">K</kbd>
-            </div>
-          </div>
+    <div className="fixed top-0 left-0 right-0 z-50">
+      <div
+        className="navigation mx-auto max-w-screen
+    md:max-w-3xl px-4 pt-4 flex h-14 items-center justify-between bg-transparent"
+      >
+        <div className="navigator flex md:text-sm text-[#909092] gap-4 md:gap-6 justify-center items-center font-medium">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/work"}>Work</Link>
+          <Link href={"/blog"}>Blog</Link>
+          <Link href={"/resume"}>Resume</Link>
         </div>
 
-        {/* Theme Switcher */}
-        <div className="theme-switcher">
-          <button
-            onClick={toggleTheme}
-            className="relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full transition-all duration-500 active:scale-90"
-          >
-            {/* Sun minimize logic */}
-            <Sun
-              className={`absolute transition-all duration-500 text-slate-50/50 size-5
+        {/* Serach bar */}
+        <div className="flex items-center">
+          <SerachBar />
+
+          {/* Theme Switcher */}
+          <div className="theme-switcher">
+            <button
+              onClick={toggleTheme}
+              className="relative flex h-12 w-12 items-center justify-center overflow-hidden rounded-full transition-all duration-500 active:scale-90"
+            >
+              {/* Sun minimize logic */}
+              <Sun
+                className={`absolute transition-all duration-500 text-slate-50/50 size-5
           ${
             currentTheme === "light"
               ? "rotate-0 scale-100 opacity-100"
               : "rotate-180 scale-0 opacity-0"
           }`}
-            />
+              />
 
-            {/* Moon minimize logic */}
-            <Moon
-              className={`absolute transition-all opacity-50 duration-500 size-5 text-slate-50/50
+              {/* Moon minimize logic */}
+              <Moon
+                className={`absolute transition-all opacity-50 duration-500 size-5 text-slate-50/50
           ${
             currentTheme === "dark"
               ? "rotate-0 scale-100 opacity-100"
               : "-rotate-180 scale-0 opacity-0"
           }`}
-            />
-          </button>
+              />
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,33 +1,81 @@
+"use client";
+import { Clip } from "@/icons/copy-icon";
+import { Tick } from "@/icons/tick-icon";
 import Image from "next/image";
+import { useState } from "react";
+import { Notification } from "./notification";
 
 export const Hero = () => {
-  return (
-    <div className="pt-10 mx-5 md:mx-0 w-full md:w-5xl">
-      <div className="relative w-fit">
-        <Image
-          className="rounded-full border-2 border-slate-100/10 size-24 hover:-translate-y-2 transition-transform duration-300"
-          src="/nishidh.png"
-          alt="nishidh"
-          width={100}
-          height={100}
-        />
+  const [clicked, setClicked] = useState(false);
 
-        {/* Ground Shadow */}
-        <div
-          className="
-            absolute
-            top-full
-            left-1/2
-            -translate-x-1/2
-            mt-2
-            w-20
-            h-2
-            bg-white/30
-            blur-md
-            rounded-full
-          "
-        />
-      </div>
+  const handleClipBoard = () => {
+    navigator.clipboard.writeText("xnishidh@gmail.com");
+    setTimeout(() => {
+      setClicked(false)
+    }, 4000)
+  };
+
+  return (
+    <div className="mx-auto w-full pt-16 md:max-w-3xl">
+      <section className="flex">
+        <div className="relative w-fit">
+          {/* nishidhx profile image */}
+          <Image
+            className="rounded-full border-2 border-slate-100/10 size-24 hover:-translate-y-2 transition-transform duration-300"
+            src="/nishidh.png"
+            alt="nishidh"
+            loading="eager"
+            width={100}
+            height={100}
+          />
+          {/* Ground Shadow for the nishidhx profile picture */}
+          <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 w-20 h-1 bg-white/30 blur-md rounded-full" />
+        </div>
+
+        <div className="flex col items-center pl-10">
+          <div>
+            <h1 className="text-lg sm:text-2xl font-semibold">
+              <span>
+                Nishidh Singh{" "}
+                <span className="text-secondary">( xnishidh )</span>
+              </span>
+            </h1>
+            <div className="text-secondary pt-2 flex items-center gap-1">
+              <div>
+                <span>
+                  Engineer <span>&#128900;</span>
+                </span>
+              </div>
+              <div>
+                <span>
+                  Tensors <span>&#128900;</span>
+                </span>
+              </div>
+              <div
+                className="group cursor-pointer"
+                onClick={() => {
+                  handleClipBoard()
+                  setClicked(true)
+                }}
+              >
+                <span className="flex items-center gap-1 group-hover:text-slate-200 duration-100 transition-colors">
+                  xnishidh@gmail.com
+                  <div>
+                    {!clicked ? (
+                      <Clip className="size-4 rotate-y-180 group-hover:text-slate-200 transition-colors duration-100" />
+                    ) : (
+                      <Tick className="size-4 text-green-500 transition-colors duration-100" />
+                    )}
+                    {
+                      clicked ? <Notification title="copied" visible={true} icon= {<Tick className="text-green-400"/>}/>: undefined
+                    }
+                  </div>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
