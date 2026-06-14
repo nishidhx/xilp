a6e4990c257c18c627df6adbd835c5dad541fd37
nee-hit476 <xnishidh.codes@gmail.com>
2026-06-14 19:22:26 +0530

    make metadata and seo changess

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,69 @@ import ClickSoundProvider from "@/components/ClickSoundProvider";
 import ReactLenis from "lenis/react";
 import { Footer } from "@/components/landing/footer";
 
+
 export const metadata: Metadata = {
-  title: "xilp",
-  description: "nishidhx portfolio",
+  title: "Nishidh Singh | Full Stack Developer",
+  description:
+    "Hey, I'm Nishihd Singh – a passionate Full Stack Developer skilled in JavaScript, TypeScript, React, Next.js, Node.js, Django, and more. Open to internships, full-time opportunities, and freelance projects.",
+  keywords: [
+    "xNishidh",
+    "xnishidh",
+    "Nishidh Singh",
+    "Nihit",
+    "Full Stack Developer",
+    "React Developer",
+    "Next.js Developer",
+    "JavaScript Developer",
+    "TypeScript",
+    "Node.js",
+    "Django",
+    "Portfolio",
+    "Web Developer",
+    "Open Source Contributor",
+    "Hackathon Developer",
+    "Nihit Singh",
+    "xnishdh.space",
+    "quebits",
+    "devrel labs",
+    "Devrel labs nihit",
+    "qibits",
+    "Quebits IT services"
+  ],
+  authors: [{ name: "Nishidh Singh", url: "https://github.com/nishidhx" }],
+  openGraph: {
+    title: "Nishidh Singh | Full Stack Developer",
+    description:
+      "Portfolio of Nishidh (Nihit) Singh – Full Stack Developer, open-source contributor, and hackathon enthusiast.",
+    url: "https://www.xnishidh.space",
+    siteName: "Nishidh's Portfolio",
+    images: [
+      {
+        url: "https://www.xnishidh.spave/nishidh.png", // Place this image in /public
+        width: 1200,
+        height: 630,
+        alt: "Nishidh Singh Portfolio Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Nishidh Singh | Full Stack Developer",
+    description:
+      "Hey, I'm Nishidh – Full Stack Developer working with React, Next.js, Django, and Node.js.",
+    site: "@xnishidh",
+    images: ["https://www.xnishidh.spave/nishidh.png"],
+  },
+  icons: {
+    icon: "/nishidh.png",
+    apple: "/nishidh.png",
+  },
+  metadataBase: new URL("https://www.xnishidh.space/"),
+  alternates: {
+    canonical: "https://xnishidh.space/",
+  },
 };
 
 export default function RootLayout({
@@ -25,8 +85,12 @@ export default function RootLayout({
       className={`h-full antialiased`}
       suppressHydrationWarning
     >
+      <head>
+        <link rel="icon" href="/nishidh.png" />
+        <meta name="robots" content="index, follow" />
+      </head>
       <body className={`font-hanken-grotesk antialiased no-scrollbar`}>
-       <ClickSoundProvider />
+        <ClickSoundProvider />
         <TooltipProvider>
           <ThemeProvider>
             <ReactLenis root>
