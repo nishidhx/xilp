a64fce21c185352dc43d4457d47b3ba26df13eb5
nee-hit476 <xnishidh.codes@gmail.com>
2026-06-21 20:03:00 +0530

    added meta data for blog page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,12 +1,48 @@
 import { BlogContent } from "@/components/blog/BlogContent";
 import { Container } from "@/components/common/container";
 import { getBlogPostBySlug } from "@/lib/blog";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import "dotenv/config"
 
 interface BlogPostPageProps {
   params: Promise<{
     slug: string;
   }>;
+}
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  //  await params
+  const { slug } = await params;
+  const post = await getBlogPostBySlug(slug);
+
+  if (!post || !post.frontmatter.isPublished) {
+    return {
+      title: 'Post Not Found',
+    };
+  }
+
+  const { title, description, image } = post.frontmatter;
+
+  return {
+    metadataBase: new URL((process.env.NEXT_PUBLIC_SERVER_URL ?? "http://localhost:3000/blog") + "/blog"),
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [image],
+      type: 'article',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+    },
+  };
 }
 
 export default async function BlogPostPage({ params }: BlogPostPageProps) {
