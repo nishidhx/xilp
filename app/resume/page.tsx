7caa37b2f0a3a2c29430c1f4bb471b7755cec907
nee-hit476 <xnishidh.codes@gmail.com>
2026-07-09 17:09:40 +0530

    fix opengraph urls for work and resume pagge

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,6 +1,47 @@
 import { Separator } from "@/components/[ui]/Separator";
 import { Container } from "@/components/common/container";
 
+
+export async function generateMetadata() {
+  const siteUrl = new URL("https://www.xnishidh.space/");
+  return {
+    metadataBase: new URL("/work", siteUrl),
+    title: "Nishidh Singh work Experience",
+    description:
+      "This page containes resume of an engineer named Nishidh Singh",
+    url: new URL("/work", siteUrl).toString(),
+    openGraph: {
+      title: "Work Experience of xNishidh",
+      description:
+        "This page containes resume of an engineer named Nishidh Singh",
+      images: [
+        {
+          url: new URL("/opengraph/resume.png", siteUrl).toString(),
+          width: 1200,
+          height: 630,
+          alt: "Nishidh Singh Work Experience",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Nishidh Singh | Full Stack Engineer",
+      description:
+        "Hey, I'm Nishidh – Engineer working with React, Next.js, Django, and Node.js.",
+      site: "@xnishidh",
+      images: [
+        {
+          url: new URL("/opengraph/resume.png", siteUrl).toString(),
+          width: 1200,
+          height: 630,
+          alt: "Nishidh Singh resume",
+        },
+      ],
+    },
+    type: "webiste",
+  };
+}
+
 export default function ResumePage() {
   return (
     <Container className="mt-20">
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,16 +1,60 @@
 import { Container } from "@/components/common/container";
 import { ExperienceCard } from "@/components/experience/Experience-card";
 
+export async function generateMetadata() {
+  const siteUrl = new URL("https://www.xnishidh.space/");
+  return {
+    metadataBase: new URL("/work", siteUrl),
+    title: "Nishidh Singh work Experience",
+    description:
+      "This page containes work and professional experience of an engineer named Nishidh Singh",
+    url: new URL("/work", siteUrl).toString(),
+    openGraph: {
+      title: "Work Experience of xNishidh",
+      description:
+        "This page containes work and professional experience of an engineer named Nishidh Singh",
+      images: [
+        {
+          url: new URL("/opengraph/experience.png", siteUrl).toString(),
+          width: 1200,
+          height: 630,
+          alt: "Nishidh Singh Work Experience",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Nishidh Singh | Full Stack Engineer",
+      description:
+        "Hey, I'm Nishidh – Engineer working with React, Next.js, Django, and Node.js.",
+      site: "@xnishidh",
+      images: [
+        {
+          url: new URL("/opengraph/experience.png", siteUrl).toString(),
+          width: 1200,
+          height: 630,
+          alt: "Nishidh Singh work experience",
+        },
+      ],
+    },
+    type: "webiste",
+  };
+}
 
 export default function Work() {
-    return <Container className="mt-20">
-        <div className="animate-in-up">
-            <h1 className="text-2xl font-bold">Work Experience</h1>
-            <p className="text-secondary">My contributions and experience across multiple companies and positions.</p>
-        </div>
-        
-            <div className="flex mt-10">
-                <ExperienceCard />
-            </div>
+  return (
+    <Container className="mt-20">
+      <div className="animate-in-up">
+        <h1 className="text-2xl font-bold">Work Experience</h1>
+        <p className="text-secondary">
+          My contributions and experience across multiple companies and
+          positions.
+        </p>
+      </div>
+
+      <div className="flex mt-10">
+        <ExperienceCard />
+      </div>
     </Container>
+  );
 }
